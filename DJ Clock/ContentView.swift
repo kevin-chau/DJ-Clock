0f1332df621e19fe4c7da355d521fc3da5f8bcda
Kevin Chau <kevin@kevinchau.com>
2020-01-28 21:59:04 -0800

    Initial Commit

---
+++ DJ Clock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DJ Clock
+//
+//  Created by Kevin Chau on 1/28/20.
+//  Copyright © 2020 Kevin Chau. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
